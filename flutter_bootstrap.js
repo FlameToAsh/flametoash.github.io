af6ffe4b24012e0eac5f4f8360daf5e332a3fc73
DeAndre Evans <chs2012grad@gmail.com>
2025-05-25 09:38:23 -0700

    trying to fix importer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1541612024"
+    serviceWorkerVersion: "3846881148"
   }
 });
