5d15300d7e707f6a29f3d97afd634cb0524dc8b3
DeAndre Evans <chs2012grad@gmail.com>
2026-01-05 18:54:48 -0800

    Add profiles feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1125753190"
+    serviceWorkerVersion: "1673109173"
   }
 });
