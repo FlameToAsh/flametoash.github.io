ed9dbe240694928a6be1877fe7f1a03fea31ccb2
DeAndre Evans <chs2012grad@gmail.com>
2026-01-05 19:55:02 -0800

    updates
    testing something

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1673109173"
+    serviceWorkerVersion: "177323358"
   }
 });
